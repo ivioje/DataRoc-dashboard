27d994d5d785f7410ace8dfc0a36f6c09526c46f
Rhoda Eboreime <rhodaivioje@gmail.com>
2025-02-08 19:07:04 +0100

    table functionalities

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,10 @@
 import { Button } from "./ui/button";
 
-const ButtonComponent = ({ text }) => {
+interface ButtonProps {
+  text: string;
+}
+
+const ButtonComponent: React.FC<ButtonProps> = ({ text }) => {
   return (
     <Button className="bg-gray-900 text-white py-2 rounded-[10px] px-4">
       {text}
